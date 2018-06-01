44e4f5d71866c81210e3e77f2b334306ed1b6948
Steven Colburn <stevencolburn@xamdes.com>
2018-06-01 10:08:50 -0700

    created a basic layout for questionaire

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,20 @@ $(function()
     $("#track-list").toggle();
   });
 
+  $("#btn-toggle-c").click(function(){
+    $("#description-c").toggle();
+  });
+
+  $("#btn-toggle-java").click(function(){
+    $("#description-java").toggle();
+  });
+
+  $("#btn-toggle-php").click(function(){
+    $("#description-php").toggle();
+  });
+
+  $("#btn-toggle-ruby").click(function(){
+    $("#description-ruby").toggle();
+  });
+
 });
